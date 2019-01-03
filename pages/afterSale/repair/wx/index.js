1cedfb65be0d0aaeab7a0069d71166d0d638ca73
luo <luo3300@qq.com>
2019-01-03 21:54:13 +0800

    no message

--- pages/afterSale/repair/wx/index.js
+++ pages/afterSale/repair/wx/index.js
@@ -1,10 +1,11 @@
-// pages/index/drive/index.js
+// pages/afterSale/repair/wx/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    title: "",
     carType: [
       "请选择",
       "车型一",
@@ -14,46 +15,75 @@ Page({
     carTypeIndex: 1,
     carDis: [
       "请选择",
-      "经销商一",
-      "经销商二",
-      "经销商三"
+      "车款一",
+      "车款二",
+      "车款三"
     ],
     carDisIndex: 1,
-    date:"2019-01-01",
-    carTime:[
+    date: "2019-01-01",
+    carTime: [
       "8:00-10:00",
       "10:00-12:00",
       "14:00-16:00",
     ],
-    carTimeIndex:1
+    carTimeIndex: 1,
+    city: [
+      "广东", "湖南", "北京"
+    ],
+    cityIndex: 1,
+    carWx:[
+      "发动机","其他"
+    ],
+    carWxIndex:1
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    var that = this;
+    var title = '';
+    if (options.type == 1) {
+      title = "预约维修";
+    } else {
+      title = "预约保养";
+    }
+    that.setData({
+      title: title
+    })
   },
-
-  bingCarType: function (e) {
+  bingWx:function(e){
+    var that = this;
+    that.setData({
+      carWxIndex: e.detail.value
+    })
+  },
+  bingCity: function(e) {
+    var that = this;
+    that.setData({
+      cityIndex: e.detail.value
+    })
+  },
+  bingCarType: function(e) {
     var that = this;
     that.setData({
       carTypeIndex: e.detail.value
     })
   },
-  bingDis: function (e) {
+  bingDis: function(e) {
     var that = this;
     that.setData({
       carDisIndex: e.detail.value
     })
   },
-  bindDate: function (e) {
+  bindDate: function(e) {
     var that = this;
     that.setData({
       date: e.detail.value
     })
   },
-  bingTime:function(e){
+  bingTime: function(e) {
     var that = this;
     that.setData({
       carTimeIndex: e.detail.value
@@ -62,49 +92,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
