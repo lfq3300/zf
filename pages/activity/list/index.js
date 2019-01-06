0af0f5cf9cb4cb6a73c5f0a06ae1a4f99badd9b8
luo <luo3300@qq.com>
2019-01-06 20:10:35 +0800

    no message

--- pages/activity/list/index.js
+++ pages/activity/list/index.js
@@ -105,7 +105,8 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
+    wx.hideNavigationBarLoading();
+    wx.stopPullDownRefresh();
   },
 
   /**
--- pages/userCenter/msg/index.js
+++ pages/userCenter/msg/index.js
@@ -1,3 +1,4 @@
+// pages/userCenter/msg/index.js
 var app = getApp();
 Page({
 
@@ -5,16 +6,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    city: [],
-    cityId: 0,
-    cityIndex: 1,
     phone: "",
     getcodetext: "获取验证码",
     getcodeStatus: true,
     ajaxStatus: true,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
   },
-  getPhoneCode: function() {
+  getPhoneCode: function () {
     var that = this;
     if (!that.data.getcodeStatus) {
       return;
@@ -23,12 +27,13 @@ Page({
     if (status) {
       return;
     }
-  
+
+
     that.setData({
       getcodeStatus: false
     })
     var downTime = 60;
-    var downTimeOut = setInterval(function() {
+    var downTimeOut = setInterval(function () {
       downTime--;
       var getcodetext = "";
       if (downTime == 0) {
@@ -46,77 +51,57 @@ Page({
       })
     }, 1000)
   },
-  phoneInput: function(e) {
+  phoneInput: function (e) {
     this.setData({
       phone: e.detail.value
     })
   },
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    var that = this;
-    app.getCity();
-    var cityOut = setInterval(function () {
-      if (app.globalData.city) {
-        clearTimeout(cityOut);
-        if (app.globalData.city.length > 0) {
-          that.setData({
-            city: app.globalData.city,
-            cityId: app.globalData.cityArr[that.data.cityIndex].id * 1,
-          });
-        }
-      }
-    }, 1000);
-  },
-
-  /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
-    wx.hideNavigationBarLoading();
-    wx.stopPullDownRefresh();
+  onPullDownRefresh: function () {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
