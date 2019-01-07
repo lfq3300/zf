15259f2c50c06dcbb28e65db148725f47871af48
luo <luo3300@qq.com>
2019-01-07 22:34:09 +0800

    no message

--- pages/success/index.js
+++ pages/success/index.js
@@ -5,13 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    msg:"提交成功"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if(options.msg){
+      this.setData({
+        msg:options.msg
+      })
+    }
     setTimeout(function () {
       wx.switchTab({
         url: '/pages/index/index'
