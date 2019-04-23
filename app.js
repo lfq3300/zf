0eaadf1862a37939ce316ec4c0a309b06cda5bc1
lfq3300 <lfq3300@qq.com>
2019-04-23 15:50:32 +0800

    no message

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ App({
     用户进来后发起微信授权 必须授权获取到用户信息后才可以
   */
   data: {
-   hostUrl: "https://zungfu2.azurewebsites.net/",
-   // hostUrl: "https://miniprogram.zfchina.com/",
+   //hostUrl: "https://zungfu2.azurewebsites.net/",
+    hostUrl: "https://miniprogram.zfchina.com/",
     appid: "wx4d69fe23e65ae0ca",
     appKey: "f3ee574e618801a984354749b2657b21",
   },
