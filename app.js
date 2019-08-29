996e24567a8abf8f92dbef057c7971810691df4b
luo <luo3300@qq.com>
2019-08-30 00:50:41 +0800

    no message

--- app.js
+++ app.js
@@ -531,5 +531,17 @@ App({
       }
     });
   },
-  
+  getCurrentPageUrlWithArgs: function () {
+    const pages = getCurrentPages()
+    const currentPage = pages[pages.length - 1]
+    const url = currentPage.route
+    const options = currentPage.options
+    let urlWithArgs = `/${url}?`
+    for (let key in options) {
+      const value = options[key]
+      urlWithArgs += `${key}=${value}&`
+    }
+    urlWithArgs = urlWithArgs.substring(0, urlWithArgs.length - 1)
+    return urlWithArgs
+  },
 });
--- pages/activity/survey/newsurvey.js
+++ pages/activity/survey/newsurvey.js
@@ -14,14 +14,22 @@ Page({
     textArrValue: [],
     pageId: '',
     hidden: true,
-    ajaxstatus: true
+    ajaxstatus: true,
+    count: "(1/1)",
+    countIndex: 0,
+    optId:0,
+    isAnswer:false,
+    optArr:[],
+    isJump:true,
+    surLen:0,
+    optIndex:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //  if (!wx.getStorageSync("surphone")){
+    // if (!wx.getStorageSync("surphone")){
     //     wx.setStorageSync("sururl", app.getCurrentPageUrlWithArgs());
     //     wx.redirectTo({
     //       url: "/pages/activity/survey/phone/index"
@@ -29,12 +37,13 @@ Page({
     // }
     var that = this;
     that.setData({
-      SurveyIdTitle: options.title,
+      SurveyIdTitle: options.title ? options.title : "",
       pageId: options.id,
       options: options
     })
     that.pageInfo(options);
   },
+
   pageInfo: function (options) {
     var that = this;
     wx.request({
@@ -44,6 +53,9 @@ Page({
         wx.hideNavigationBarLoading();
         wx.stopPullDownRefresh();
         if (res.statusCode == 200 && res.data.success) {
+          that.setData({
+            count: "(1/" + res.data.result.length + ")",
+          })
           var textArrStatus = [];
           var textArrValue = [];
           res.data.result.forEach((item, index) => {
@@ -66,47 +78,49 @@ Page({
             }
             textArrValue.push('');
           })
-          console.log(res.data.result[0]);
           that.setData({
             SurveyIdTitle: res.data.result[0].surveyIdName,
             surveyArr: res.data.result,
             textArrStatus: textArrStatus,
             textArrValue: textArrValue,
-            loginhidde: false
+            loginhidde: false,
+            optArr:[res.data.result[0].id],
+            surLen: res.data.result.length,
+            optId: [res.data.result[0].id] 
           });
         }
       },
       fail: function (res) {
         wx.hideNavigationBarLoading();
         wx.stopPullDownRefresh();
-        console.log('--fail--');
-        console.log(res)
       }
     })
   },
   bindRadioOption: function (e) {
-    console.log(e);
     var questionid = e.target.dataset.questionid;
     var index = e.target.dataset.index;
     this.setFromData(e, questionid, index);
+    console.log(e);
+    this.setData({
+      isAnswer:true
+    })
   },
 
   bindCheckboxOption: function (e) {
-    console.log(e);
     var questionid = e.target.dataset.questionid;
     var index = e.target.dataset.index;
     this.setFromData(e, questionid, index, true);
+    this.setData({
+      isAnswer: true
+    })
   },
   setFromData: function (e, questionid, index, status = false) {
-    var value = e.detail.value;
+    var value = e.detail.value
+    this.jumpOpt(value, questionid, index);
     var questions = this.data.questions;
-    console.log(questions);
     var len = questions.length;
-    console.log(len);
     var textArrStatus = this.data.textArrStatus;
-    console.log(textArrStatus);
     var textArrValue = this.data.textArrValue;
-    console.log(textArrValue);
     for (var i = 0; i < len; i++) {
       if (parseInt(questions[i].questionId) === parseInt(questionid)) {
         delete questions[i];
@@ -288,6 +302,96 @@ Page({
       }
     })
   },
+
+  //跳题目 
+  jumpOpt:function(value,thisid,index){
+    if (value instanceof Array){
+      value = value[0];
+    }
+    var valueArr = value.split("-|-");
+    var nextId = valueArr.pop();
+    console.log(nextId);
+    if (nextId == "null"){
+      console.log("为空"); 
+      var opt = this.data.surveyArr;
+      console.log(opt);
+      //nextId 就是 下一个
+      console.log(index);
+      nextId = opt[index+1].id;
+    }else{
+      console.log("不为空"); 
+    }
+    var isJump = this.data.isJump;
+    var optArr = this.data.optArr;
+    if (isJump){
+      this.setData({
+        isJump: false
+      });
+    }else{
+      // 删除最后一个  在添加 
+      optArr.pop();
+    }
+    optArr.push(parseInt(nextId));
+    this.setData({
+      optArr: optArr
+    });
+    console.log(optArr);
+  },
+  next: function () {
+    wx.hideToast();
+    var isAnswer = this.data.isAnswer;
+    if(isAnswer){
+      this.timu(true);
+    }else{
+      wx.showToast({
+        title:"当前题目未回答",
+        icon:'none',
+        duration:1500
+      })
+    }
+    this.setData({
+      isAnswer:false
+    })
+  },
+  prev: function () {
+    this.timu(false);
+  },
+  timu: function (status) {
+    this.setData({
+      isJump:true
+    })
+    var optArr = this.data.optArr;
+    if (!status) {
+      optArr.pop();
+      //需要将 optArr 去掉最后一个 返回上一题
+      this.setData({
+        optArr: optArr
+      })
+      console.log("上一题");
+    }
+    console.log(optArr);
+    var nextId = optArr[optArr.length-1];
+    console.log(optArr);
+    console.log(nextId);
+    var surveyArr = this.data.surveyArr;
+    var optId = 0;
+    var countIndex = 0;
+    for (var i = 0; i < surveyArr.length;i++){
+      if (surveyArr[i].id == nextId){
+        optId = nextId;
+        break;
+      }
+      countIndex += 1;
+    }
+    
+    console.log(this.data);
+    var count = "(" + countIndex + 1 + "/" + surveyArr.length + ")";
+    this.setData({
+      optId: optId,
+      count: count,
+      optIndex: countIndex
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -299,8 +403,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    that.pageInfo(that.setData.options);
+
   },
 
   /**
